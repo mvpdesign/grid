b5c37e9aacd1edbf3bb0b48cd4e74f127e8aa5cb
Brock Riemenschneider <brock@mvpdesign.com>
2013-08-02 08:50:42 -0500

    Added google analytics

--- index.php
+++ index.php
@@ -80,5 +80,16 @@
         <script src="<?= Assets::path('scripts/lib/respond.min.js'); ?>"></script>
         <script src="<?= Assets::path('scripts/lib/html2canvas.js'); ?>"></script>
         <script src="<?= Assets::path('scripts/site.js'); ?>"></script>
+                
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+        
+          ga('create', 'UA-42869249-1', 'visualgridref.com');
+          ga('send', 'pageview');
+        
+        </script>
     </body>
 </html>
